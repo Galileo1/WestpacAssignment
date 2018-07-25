ba3e8fcb06e09dcd9ec8a288c4714b16fdf7050c
Varun Gaur <gaur.var@gmail.com>
2018-07-26 00:00:44 +0530

    adding remotewebdriver configuration

--- src/test/java/org/galileo1/bdd/driver/DriverConfiguration.java
+++ src/test/java/org/galileo1/bdd/driver/DriverConfiguration.java
@@ -1,9 +1,15 @@
 package org.galileo1.bdd.driver;
 
+
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -33,6 +39,13 @@ public class DriverConfiguration  {
                 return new ChromeDriver(setChromeCapabilities());
             case "firefox":
                 return new FirefoxDriver();
+            case "remote":
+                try {
+                    return new RemoteWebDriver(new URL("http://selenium-hub:4444/wd/hub"), setChromeCapabilities());
+                } catch (MalformedURLException e) {
+                    
+                    e.printStackTrace();
+                }
             default:
                 return new ChromeDriver(setChromeCapabilities());
         }
@@ -43,6 +56,8 @@ public class DriverConfiguration  {
         chromeOptions.addArguments("--headless", "window-size=1024,768", "--no-sandbox", "--disable-gpu");
         return chromeOptions;
     }
+
+
     
     //TODO: This is ugly. Need to write it in a better way.
     private void getRunningEnvironment() {
